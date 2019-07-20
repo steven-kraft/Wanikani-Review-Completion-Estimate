8a7531094c66702f514a59df0f6eee098ceb2a2e
Steven-Kraft <steven.kraftjp@gmail.com>
2019-07-20 11:26:52 -0400

    Changed minute and seconds to an easier to read format

--- main.js
+++ main.js
@@ -109,7 +109,7 @@ function formatAMPM(date) {
 function millisToMinutesAndSeconds(millis) {
   var minutes = Math.floor(millis / 60000);
   var seconds = ((millis % 60000) / 1000).toFixed(0);
-  return minutes + ":" + (seconds < 10 ? '0' : '') + seconds;
+  return minutes + "m " + seconds + "s";
 }
 
 function updateTippy() {
